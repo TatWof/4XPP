d4169f36c6f600db3dd4b1331b06d7cdaaf12849
TatWof <104392674+TatWof@users.noreply.github.com>
2022-07-26 19:51:11 -0700

    resource.h setup

--- gameCode/bottomLevel/map.h
+++ gameCode/bottomLevel/map.h
@@ -6,5 +6,4 @@
 
 Tile map[X_MaxSize][Y_MaxSize]{};
 
-
 #endif
--- gameCode/bottomLevel/resource.h
+++ gameCode/bottomLevel/resource.h
@@ -1,12 +1,52 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
-enum baseResourceType{};
+Resource resources[]
+{
+{"Wood", Construction, 'W'},
+{"Stone", Construction, 'S'},
+{"Gold", Gold, 'G'},
+{"Cotton", Luxury, 'C'},
+{"Horse", Strategtic, 'H'},
+{"Iron", Strategtic, 'H'},
+{"NoResource", noResourceType, ' '}
+};
+
+enum baseResourceType{baseInvaildResource = 0, noResourceType = 1, Gold = 2, Construction = 3, Luxury = 4, Strategtic = 5};
 
 struct Resource
 {
-    
+    std::string name;
+    char key;
+    baseResourceType resourceType;
+
+    Resource(std::string name, baseResourceType resourceType, char key)
+    {
+    this->name = name;
+    this->resourceType = resourceType;
+    this->key = key;
+    }
+
+
+    bool operator==(baseResourceType compare)
+    {
+        if (resourceType == compare) return true;
+        
+        return false;
+    }
+    bool operator==(Resource compare)
+    {
+        if (name == compare.name) return true;
+
+        return false;
+    }
 };
+
+    std::ostream& operator<<(std::ostream out, Resource r)
+    {
+        out << r.name;
+        return out;
+    }
 
 
 #endif
